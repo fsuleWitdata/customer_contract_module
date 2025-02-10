4c4419f4887bf50a2b1c3796050966ea5838b4e8
fsuleWitdata <fsule@witdata.com.ar>
2025-02-10 16:27:51 -0300

    description

--- customer_contract_module/__manifest__.py
+++ customer_contract_module/__manifest__.py
@@ -3,7 +3,13 @@
     'version': '1.0',
     'category': 'Sales',
     'author': 'witdata',
+    "images":['static/description/main_screenshot.png'],
+    'summary': 'Module to save contracts with clients.',
+    'description': """
+        App Module to save contracts with clients, hours, date range, status.
+    """,
     'depends': ['base', 'contacts'],
+    "license" : "	AGPL-3",
     'data': [
         'views/customer_contract_view.xml',
         'views/customer_contract_menu.xml',
@@ -12,5 +18,6 @@
         'security/security.xml',
     ],
     'installable': True,
+    'auto_install': False,
     'application': True,
 }
